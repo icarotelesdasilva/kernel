8c30b84e9077cd26d6561a4c09de5f645a224423
icarotelesdasilva <telesicaro04@gmail.com>
2026-07-11 10:11:46 -0300

    primeira versão

---
+++ boot/boot.asm
@@ -0,0 +1,34 @@
+ bits 32
+
+section .multiboot
+
+align 4
+
+dd 0x1BADB002
+dd 0x00000003
+
+dd -(0x1BADB002 + 0x00000003)
+
+section .bss 
+
+align 16
+stack_bottom:
+resb 16384
+stack_top:
+
+section .text
+global _start
+extern kmain
+
+_start:
+
+mov esp, stack_top
+call kmain
+cli
+
+.hang:
+
+hlt 
+
+jmp .hang 
+
---
+++ boot/src/icaro.c
@@ -0,0 +1,7 @@
+void kmain(void)  {
+
+while(1);
+
+}
+
+
